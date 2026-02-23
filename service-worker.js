9d9c6575d3971ce99e6468eb043b7a719f104d7c
Infingardo <filippobianchi@fastwebnet.it>
2026-02-23 23:33:14 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,70 +1,31 @@
-const CACHE_NAME = 'olga-olgim-v5.7.21';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+const CACHE_NAME = 'olga-olgim-v5.7.22';
+const urlsToCache = ['./', './index.html', './manifest.json'];
 
-// Install
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Cache aperta');
-        return cache.addAll(urlsToCache);
-      })
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
   self.skipWaiting();
 });
 
-// Fetch
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        
-        // Clone request
-        const fetchRequest = event.request.clone();
-        
-        return fetch(fetchRequest).then(response => {
-          // Check if valid response
-          if (!response || response.status !== 200 || response.type !== 'basic') {
-            return response;
-          }
-          
-          // Clone response
-          const responseToCache = response.clone();
-          
-          caches.open(CACHE_NAME)
-            .then(cache => {
-              cache.put(event.request, responseToCache);
-            });
-          
-          return response;
-        });
-      })
+    caches.match(event.request).then(response => {
+      if (response) return response;
+      const fetchRequest = event.request.clone();
+      return fetch(fetchRequest).then(response => {
+        if (!response || response.status !== 200 || response.type !== 'basic') return response;
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseToCache));
+        return response;
+      });
+    })
   );
 });
 
-// Activate
 self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+    caches.keys().then(cacheNames =>
+      Promise.all(cacheNames.filter(n => n !== CACHE_NAME).map(n => caches.delete(n)))
+    )
   );
-  
   return self.clients.claim();
 });
