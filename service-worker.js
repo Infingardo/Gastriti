56be35e81f534fd0f699a257cb61d11e83dbefe2
Infingardo <filippobianchi@fastwebnet.it>
2026-03-05 21:40:43 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'olga-olgim-v5.7.27';
+const CACHE_NAME = 'olga-olgim-v5.7.28';
 const urlsToCache = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', event => {
